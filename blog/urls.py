16b8abeb4bd36cb8a02c1905d212a00acbb933a7
ycoodr <ycoodr@gmail.com>
2022-06-19 15:04:28 +0300

    форма отправки

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('specific', views.specific, name='specific'),
-    path('article/<int:article_id>', views.article, name='article' )
+   # path('article/<int:article_id>', views.article, name='article' )
+    path('showEmployeeForm', views.showEmployeeForm, name="showEmployeeForm"),
+    path('createEmpoyee', views.createEmployee, name="createEmployee")
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from .models import Employee
 
 # Create your views here.
 
@@ -10,5 +11,22 @@ def specific(request):
     list1 = [1, 2, 3, 4]
     return HttpResponse(list1)
 
-def article(request, article_id):
-    return render(request, 'blog/index.html', {'article_id': article_id})
+# def article(request, article_id):
+#     return render(request, 'blog/index.html', {'article_id': article_id})
+def showEmployeeForm(request):
+    return render(request, 'blog/employeeForm.html')
+
+def createEmployee(request):
+    if request.method == "POST":
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        job = request.POST.get('job')
+        salary = request.POST.get('salary')
+        employee = Employee()
+        employee.name = name
+        employee.email = email
+        employee.job = job
+        employee.salary = salary
+        employee.save()
+        return redirect('index')
+    return redirect('index')
