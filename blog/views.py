92b7a85f71a892cd84d2f23292e73e1125a60b02
ycoodr <ycoodr@gmail.com>
2022-06-19 14:01:15 +0300

    тест роутера респонс

---
+++ blog/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def index(request):
+    return HttpResponse("This is my first url")
+
+def specific(request):
+    list1 = [1, 2, 3, 4]
+    return HttpResponse(list1)
+
+def article(request, article_id):
+    return HttpResponse(article_id)
