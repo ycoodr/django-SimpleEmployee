6620e7e582097d8a1a43e77af8f0331b4a2288e8
ycoodr <ycoodr@gmail.com>
2022-06-19 16:33:46 +0300

    изменение стиля формы

--- blog/urls.py
+++ blog/urls.py
@@ -7,5 +7,8 @@ urlpatterns = [
     path('specific', views.specific, name='specific'),
    # path('article/<int:article_id>', views.article, name='article' )
     path('showEmployeeForm', views.showEmployeeForm, name="showEmployeeForm"),
-    path('createEmpoyee', views.createEmployee, name="createEmployee")
+    path('createEmpoyee', views.createEmployee, name="createEmployee"),
+    path('editEmployeeForm/<int:employee_id>', views.editEmployeeForm, name="editEmployeeForm"),
+    path('editEmployee', views.editEmployee, name="editEmployee"),
+    path('deleteEmployee/<int:employee_id>', views.deleteEmployee, name="deleteEmployee")
 ]
--- blog/views.py
+++ blog/views.py
@@ -5,7 +5,8 @@ from .models import Employee
 # Create your views here.
 
 def index(request):
-    return HttpResponse("This is my first url")
+    employees = Employee.objects.all()
+    return render(request, 'blog/index.html', {'employees': employees})
 
 def specific(request):
     list1 = [1, 2, 3, 4]
@@ -29,4 +30,31 @@ def createEmployee(request):
         employee.salary = salary
         employee.save()
         return redirect('index')
+    return redirect('index')
+
+
+def editEmployeeForm(request, employee_id):
+    employee = Employee.objects.get(pk=employee_id)
+    return render(request, 'blog/editEmployeeForm.html', {'employee': employee})
+
+def editEmployee(request):
+    if request.method == "POST":
+        id = request.POST.get('id')
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        job = request.POST.get('job')
+        salary = request.POST.get('salary')
+
+        employee = Employee.objects.get(pk=id)
+        employee.name = name
+        employee.email = email
+        employee.job = job
+        employee.salary = salary
+        employee.save()
+        return redirect('index')
+    return redirect('index')
+
+def deleteEmployee(request, employee_id):
+    employee = Employee.objects.get(pk=employee_id)
+    employee.delete()
     return redirect('index')
